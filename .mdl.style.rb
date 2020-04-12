82a9046a9c9f35a5b90e4387eca1fea5165d65df
Alexander Popov <alex.wayfer@gmail.com>
2020-04-12 05:02:21 +0300

    Fix offenses from `markdownlint`

---
+++ .mdl.style.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+all
+
+rule 'MD007', indent: 4
+rule 'MD024', allow_different_nesting: true
+rule 'MD029', style: 'ordered'
+rule 'MD030', ul_single: 3, ul_multi: 3, ol_single: 2, ol_multi: 2
